7dee6b070b1e7ed2446b91b10bfc04229bffbc98
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-10-06 19:31:31 -0600

    ready for turn in?

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,6 +1,6 @@
 //update current year
 let todaysYear = new Date().getFullYear();
-document.querySelector('footer span').textContent = todaysYear;
+document.querySelector('#year').textContent = todaysYear;
 
 //update last modified
 document.querySelector('#lastModified').textContent = "Last Modified: " + document.lastModified
