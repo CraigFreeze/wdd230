fdf90934e471b90a634c680d6cfe3f2596467a51
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-11-09 12:26:51 -0700

    working links

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,32 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const baseURL = "https://craigfreeze.github.io/wdd230/";
+const linksURL = "https://craigfreeze.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
-  }
-  
-  getLinks();
+    displayLinks(data.lessons);
+}
+
+function displayLinks(weeks) {
+    let ul = document.querySelector(".card1 ul");
+
+    weeks.forEach((week) => {
+        const weekEl = document.createElement("li");
+        let weekHTML = `${week.lesson}`
+        let isFirst = true;
+        week.links.forEach((element) => {
+            if (!isFirst) {
+                weekHTML += " | "
+            } else {
+                isFirst = false;
+            }
+            weekHTML += ` <a href="${element.url}">${element.title}</a>`;
+        });
+        weekEl.innerHTML = weekHTML;
+        ul.append(weekEl)
+    });
+}
+
+getLinks();
+
