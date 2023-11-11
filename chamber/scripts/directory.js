5be6f6e40c3e93c91b785bd3af8a5a44af93ffad
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-11-10 19:14:14 -0700

    update directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -14,14 +14,13 @@ async function getBusinessData() {
 }
 
 const displayDirectory = (businesses) => {
-    console.log("test:");
     businesses.forEach((business) => {
         let card = document.createElement("section");
         let name = document.createElement("h2");
         let paragraph1 = document.createElement("p");
         let paragraph2 = document.createElement("p");
         let paragraph3 = document.createElement("p");
-        let paragraph4 = document.createElement("p");
+        // let paragraph4 = document.createElement("p");
         let paragraph5 = document.createElement("p");
         let paragraph6 = document.createElement("p");
         let paragraph7 = document.createElement("p");
@@ -29,16 +28,16 @@ const displayDirectory = (businesses) => {
         let portrait = document.createElement("img");
 
         name.textContent = `${business.name}`;
-        paragraph1.textContent = `Address: ${business.address}`;
-        paragraph2.textContent = `Phone: ${business.phone}`;
-        paragraph3.textContent = `website: ${business.website}`;
-        paragraph4.textContent = `imageFile: ${business.imageFile}`;
-        paragraph5.textContent = `membershipLevel: ${business.membershipLevel}`;
-        paragraph6.textContent = `joinDate: ${business.joinDate}`;
+        paragraph1.textContent = `${business.address}`;
+        paragraph2.textContent = `${business.phone}`;
+        paragraph3.innerHTML = `<a href=${business.website}>${business.website}</a>`;
+        // paragraph4.textContent = `imageFile: ${business.imageFile}`;
+        paragraph5.textContent = `${business.membershipLevel}`;
+        paragraph6.textContent = `${business.joinDate}`;
 
 
-        // portrait.setAttribute("src", business.imageFile);
-        portrait.setAttribute("src", "images/directory/1business.webp");
+        portrait.setAttribute("src", business.imageFile);
+        // portrait.setAttribute("src", "images/directory/1business.webp");
         portrait.setAttribute("alt", `Logo of ${business.name}`);
         portrait.setAttribute("loading", "lazy");
         portrait.setAttribute("width", "340");
@@ -49,7 +48,7 @@ const displayDirectory = (businesses) => {
         card.appendChild(paragraph1);
         card.appendChild(paragraph2);
         card.appendChild(paragraph3);
-        card.appendChild(paragraph4);
+        // card.appendChild(paragraph4);
         card.appendChild(paragraph5);
         card.appendChild(paragraph6);
         card.appendChild(paragraph7);
