dfabfa6c5c8f5b407f88aab80b5bed4d64325bc8
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-11-25 17:41:26 -0700

    spotlight added

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -11,48 +11,43 @@ async function getBusinessData() {
     }
 }
 
+
+
 // Member Spotlight
 const displaySpotlight = (businesses) => {
     businesses.forEach((business) => {
-        if(business.membershipLevel == 'Silver' || business.membershipLevel == 'Gold'){
-            console.log(business.name)
+        if (business.membershipLevel == 'Silver' || business.membershipLevel == 'Gold') {
+            let card = document.createElement("section");
+            let spotlightTitle = document.createElement("h2");
+            let portrait = document.createElement("img");
+            let name = document.createElement("h3");
+            let paragraph1 = document.createElement("p");
+            let paragraph2 = document.createElement("p");
+            let paragraph3 = document.createElement("p");
+
+            spotlightTitle.textContent = `Member Spotlight - ${business.membershipLevel}`;
+            name.textContent = `${business.name}`;
+            paragraph1.innerHTML = `<br> ${business.address}`;
+            paragraph2.textContent = `${business.phone}`;
+            paragraph3.innerHTML = `<a href=${business.website}>${business.website}</a>`;
+            portrait.setAttribute("src", business.imageFile);
+            portrait.setAttribute("alt", `Logo of ${business.name}`);
+            portrait.setAttribute("loading", "lazy");
+            portrait.setAttribute("width", "340");
+            portrait.setAttribute("height", "440");
+
+            card.appendChild(spotlightTitle);
+            card.appendChild(portrait);
+            card.appendChild(name);
+            card.appendChild(paragraph1);
+            card.appendChild(paragraph2);
+            card.appendChild(paragraph3);
+
+            card.classList.add("card");
+
+
+            cards.append(card);
         }
-
-        // let card = document.createElement("section");
-        // let name = document.createElement("h2");
-        // let paragraph1 = document.createElement("p");
-        // let paragraph2 = document.createElement("p");
-        // let paragraph3 = document.createElement("p");
-        // let paragraph5 = document.createElement("p");
-        // let paragraph6 = document.createElement("p");
-
-        // let portrait = document.createElement("img");
-
-        // name.textContent = `${business.name}`;
-        // paragraph1.textContent = `${business.address}`;
-        // paragraph2.textContent = `${business.phone}`;
-        // paragraph3.innerHTML = `<a href=${business.website}>${business.website}</a>`;
-        // paragraph5.textContent = `${business.membershipLevel} Member`;
-        // paragraph6.textContent = `${business.joinDate}`;
-
-
-        // portrait.setAttribute("src", business.imageFile);
-        // portrait.setAttribute("alt", `Logo of ${business.name}`);
-        // portrait.setAttribute("loading", "lazy");
-        // portrait.setAttribute("width", "340");
-        // portrait.setAttribute("height", "440");
-
-        // card.appendChild(portrait);
-        // card.appendChild(name);
-        // card.appendChild(paragraph1);
-        // card.appendChild(paragraph2);
-        // card.appendChild(paragraph3);
-        // card.appendChild(paragraph5);
-        // card.appendChild(paragraph6);
-
-        // card.classList.add("directory")
-
-        // cards.append(card)
     });
 }
 
