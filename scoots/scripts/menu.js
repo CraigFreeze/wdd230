507011158389b792974f2b1f7e328b7905029991
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-12-09 00:35:23 -0700

    updated home page

--- scoots/scripts/menu.js
+++ scoots/scripts/menu.js
@@ -1,6 +1,7 @@
 // Store the selected elements that we are going to use. 
 const mainnav = document.querySelector('.nav-links-container')
 const hambutton = document.querySelector('#menu');
+const reserve = document.querySelector('#reserve');
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
