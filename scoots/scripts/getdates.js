677c778012b2c7e876fcdc9dcd2307b8f774893b
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-12-09 22:57:46 -0700

    table changes

--- scoots/scripts/getdates.js
+++ scoots/scripts/getdates.js
@@ -3,5 +3,5 @@ let todaysYear = new Date().getFullYear();
 document.querySelector('#year').textContent = todaysYear;
 
 //update last modified
-document.querySelector('#lastModified').textContent = "Last Modified: " + document.lastModified
+// document.querySelector('#lastModified').textContent = "Last Modified: " + document.lastModified
 
--- scoots/scripts/rental-pricing.js
+++ scoots/scripts/rental-pricing.js
@@ -1,31 +1,48 @@
-rentals.forEach(rental => {
-    //Create
-    let table = document.querySelector("#forecastList");
-    let tr = document.createElement("tr");
-    let tdType = document.createElement("td");
-    let tdPersons = document.createElement("td");
-    let tdHalfR = document.createElement("td");
-    let tdFullR = document.createElement("td");
-    let tdHalfW = document.createElement("td");
-    let tdFullW = document.createElement("td");
+const url = 'https://craigfreeze.github.io/wdd230/scoots/data/rental-pricing.json';
 
-    //Assign Values
-    tdType.textContent = rental.rentalType;
-    tdPersons.textContent = rental.maxPeople;
-    tdHalfR.textContent = rental.reserveHalf;
-    tdFullR.textContent = rental.reserveFull;
-    tdHalfW.textContent = rental.walkInHalf;
-    tdFullW.textContent = rental.walkInFull;
+async function getRentalData() {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+        displayRentalData(data);
+    } else {
+        console.log(response.status);
+    }
+}
 
-    //Append
-    tr.append(tdType);
-    tr.append(tdPersons);
-    tr.append(tdHalfR);
-    tr.append(tdFullR);
-    tr.append(tdHalfW);
-    tr.append(tdFullW);
-    //Append row to table
-    table.append(tr);
-});
+function displayRentalData(data) {
+    data.rentals.forEach(rental => {
+        //Create
+        let table = document.querySelector(".rental-pricing-table");
+        let tr = document.createElement("tr");
+        let tdType = document.createElement("td");
+        let tdPersons = document.createElement("td");
+        let tdHalfR = document.createElement("td");
+        let tdFullR = document.createElement("td");
+        let tdHalfW = document.createElement("td");
+        let tdFullW = document.createElement("td");
+
+        //Assign Values
+        tdType.textContent = rental.rentalType;
+        tdPersons.textContent = rental.maxPeople;
+        tdHalfR.textContent = rental.reserveHalf;
+        tdFullR.textContent = rental.reserveFull;
+        tdHalfW.textContent = rental.walkInHalf;
+        tdFullW.textContent = rental.walkInFull;
+
+        //Append
+        tr.append(tdType);
+        tr.append(tdPersons);
+        tr.append(tdHalfR);
+        tr.append(tdFullR);
+        tr.append(tdHalfW);
+        tr.append(tdFullW);
+        //Append row to table
+        table.append(tr);
+    });
+}
+
+getRentalData()
 
 
