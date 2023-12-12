d4a8e1b0411ac9cf823874e88c5871bcf18fa59f
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-12-11 20:24:21 -0700

    working carousel with labels

--- scoots/scripts/carousel.js
+++ scoots/scripts/carousel.js
@@ -1,9 +1,10 @@
 const buttons = document.querySelectorAll(".carousel-btn");
 const images = document.querySelectorAll(".slide");
+const descriptionEl = document.querySelector(".carousel-description");
+descriptionEl.textContent = document.querySelector(".img-active img").alt;
 let index = 0;
 
 buttons.forEach(button => {
-
     button.addEventListener('click', () => {
         images[index].classList.remove("img-active");
         if (button.classList.contains("prev")) {
@@ -21,5 +22,6 @@ buttons.forEach(button => {
             }
             images[index].classList.add("img-active");
         }
+        descriptionEl.textContent = document.querySelector(".img-active img").alt;
     });
 });
