399ca2aedeba5c5f8b57c2777721d58c1c7d3389
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2023-12-11 19:07:47 -0700

    working carousel

---
+++ scoots/scripts/carousel.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".carousel-btn");
+const images = document.querySelectorAll(".slide");
+let index = 0;
+
+buttons.forEach(button => {
+
+    button.addEventListener('click', () => {
+        images[index].classList.remove("img-active");
+        if (button.classList.contains("prev")) {
+            if (index == 0) {
+                index = images.length - 1;
+            } else {
+                index--;
+            }
+            images[index].classList.add("img-active");
+        } else if (button.classList.contains("next")) {
+            if (index == (images.length - 1)) {
+                index = 0;
+            } else {
+                index++;
+            }
+            images[index].classList.add("img-active");
+        }
+    });
+});
